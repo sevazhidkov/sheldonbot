d7b48bce4d93d038add907f6d4f88ecccc3214ac
Seva Zhidkov <zhidkovseva@gmail.com>
2015-07-26 16:09:46 +0400

    Add basic structure of bot file

---
+++ sheldon/bot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Lises team
+@contact: zhidkovseva@gmail.com
+@license: The MIT license
+
+Copyright (C) 2015
+"""
+
+__author__ = 'Lises team'
+__version__ = '0.1'
+__email__ = 'zhidkovseva@gmail.com'
+
+
+class Sheldon():
+    pass
+
+
+class Plugin():
+    pass
+
+
+class Adapter():
+    pass
