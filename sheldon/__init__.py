671d1a71ac746aa752fbb77071b0f8c7037f39ec
Seva Zhidkov <zhidkovseva@gmail.com>
2015-07-28 17:48:12 +0400

    Refactor imports in __init__ of bot

--- sheldon/__init__.py
+++ sheldon/__init__.py
@@ -12,21 +12,11 @@ __author__ = 'Lises team'
 __version__ = '0.1'
 __email__ = 'zhidkovseva@gmail.com'
 
-import sys
-
 # Bot file contains bot's main class - Sheldon
 # Utils folder contains scripts for more
 # comfortable work with sending and parsing
 # messages. For example, script for downloading
 # files by url.
-#
-# If bot using Python 2.7 or lower, than we use
-# old syntax of importing modules.
-# It's needed for Python 2 compatibility
 
-if sys.version_info[0] == 2:
-    from bot import *
-    from utils import *
-else:
-    from .bot import *
-    from .utils import *
+from sheldon.bot import *
+from sheldon.utils import *
