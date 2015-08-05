41b66fa93edc1c5dcd3d4cc28750971fc33eb6a1
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-05 13:01:15 +0400

    Create attachment class

--- sheldon/basic_classes.py
+++ sheldon/basic_classes.py
@@ -55,14 +55,50 @@ class Adapter:
         :param message: Message object
         :return: True or False - result of sending
         """
-        pass
+        try:
+            self.module.send_message(
+                message_text=message.text,
+
+            )
 
 
 class Message:
     """
     Class for every message: incoming and outcoming.
     """
-    def __init__(self, message_text, adapter):
+
+    def __init__(self, message_text, adapter, message_attachments=[]):
+        """
+        Init new message
+
+        :param message_text: string, text of new message
+        :param adapter: Adapter object, current adapter
+        :param message_attachments: list of Attachment objects
+        :return:
+        """
         self.text = message_text
         self.adapter = adapter
+        self.attachments = message_attachments
+
+
+class Attachment:
+    """
+    Class for every attachment: incoming and outcoming/
+    """
+
+    def __init__(self, attachment_type, attachment_path=None,
+                 attachment_text=''):
+        """
+        Init new attachment.
+
+        :param attachment_type: 'photo', 'video' and other things,
+                                supported by adapter
+        :param attachment_path: path to photo, video etc.
+        :param attachment_text: optional text of attachment - may
+                                be сaption or something another
+        :return:
+        """
+        self.type = attachment_type.lower()
+        self.path = attachment_path
+        self.text = attachment_text
 
