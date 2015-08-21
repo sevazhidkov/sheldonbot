5647d9d787a3257df614febc2d323c618bc05f10
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-21 19:38:06 +0400

    Create file for modules manager

---
+++ sheldon/manager.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Manager for plugins and adapters
+
+@author: Lises team
+@contact: zhidkovseva@gmail.com
+@license: The MIT license
+
+Copyright (C) 2015
+"""
+
+
+class ModuleManager:
+    pass
+
+
+class PluginManager(ModuleManager):
+    pass
+
+
+class AdapterManager(ModuleManager):
+    pass
--- sheldon/modules.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@author: Lises team
-@contact: zhidkovseva@gmail.com
-@license: The MIT license
-
-Copyright (C) 2015
-"""
