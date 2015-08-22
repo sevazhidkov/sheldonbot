1851190543d24d6f4c26a5d7a3a04f56aeba511d
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-22 11:06:21 +0400

    Create decorator for catching plugin errors

--- sheldon/exceptions.py
+++ sheldon/exceptions.py
@@ -7,3 +7,22 @@
 
 Copyright (C) 2015
 """
+
+from sheldon.utils import logger
+
+
+def catch_plugin_errors(plugin_call_function):
+    """
+    Catch all plugin exceptions and log it
+
+    :param plugin_call_function: function with calling user plugin
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            plugin_call_function(*args, **kwargs)
+        except Exception as error:
+            error_message = str(error.__traceback__)
+            logger.error_log_message('Plugin error: \n' + error_message)
+
+    return wrapper
