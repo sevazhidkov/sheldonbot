3bcfb83614484ff6ad5d6200669e22d4624d938e
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-22 20:18:22 +0400

    Create basic of Plugin class

--- sheldon/manager.py
+++ sheldon/manager.py
@@ -13,3 +13,18 @@ Copyright (C) 2015
 
 class PluginsManager:
     pass
+
+
+class Plugin:
+    def __init__(self, name, module, hooks):
+        """
+        Create new plugin
+
+        :param name: string, module name
+        :param module: module, imported plugin module
+        :param hooks: list of Hook objects
+        :return:
+        """
+        self.name = name
+        self.module = module
+        self.hooks = hooks
