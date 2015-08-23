e9fa8a3b9b81a84652467e656c4ed47b7ed9a2cd
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-23 11:11:48 +0400

    Create example console adapter

---
+++ adapters/console.py
@@ -0,0 +1,21 @@
+"""
+Console adapter.
+Getting messages from stdin and
+sending it to stdout.
+
+name: console
+config:
+  - SHELDON_CONSOLE_PROMPT
+"""
+
+
+def on_start(bot):
+    pass
+
+
+def get_messages(bot):
+    pass
+
+
+def send_message(Message, bot):
+    pass
