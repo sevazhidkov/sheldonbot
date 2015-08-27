176ea7442e8016dcae49b45a6f13834295b2e57b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-27 13:51:10 +0400

    Add get_messages function for console adapter

--- adapters/console.py
+++ adapters/console.py
@@ -1,21 +1,76 @@
 """
 Console adapter.
+
 Getting messages from stdin and
 sending it to stdout.
 
-name: console
-config:
-  - SHELDON_CONSOLE_PROMPT
+Example adapter
+
+# Config must be at the end of __doc__
+# and have one blank line before it.
+# Also config must be valid YAML document.
+# Comments must be after '#'
+name: console # Name of adapter, lowercase, match with
+              # file or package name.
+config:                           # Config variable needed to set
+  - SHELDON_CONSOLE_PROMPT: '>>>' # in environment or in config.py).
+                                  # You can set default values after colon.
 """
+from sheldon import IncomingMessage, Attachment
+from random import randint
+from os import getlogin
 
-
-def on_start(bot):
-    pass
+# Code running on adapter loading, may be here
 
 
 def get_messages(bot):
-    pass
+    while True:
+        text = input('Enter message: ')
+
+        attachments = []
+        # For our example, attachment will be looking like:
+        # '[type]_[path]'
+        received_attachments = input(
+            'Enter a comma-separated attachment'
+            '(<type>_<path>):'
+        ).split(',')
+
+        # If user don't enter attachment,
+        # message_attachments will be empty
+        if received_attachments[0]:
+            for attachment in received_attachments:
+                # Parse incoming attachment:
+                # attachment_data[0] will be attachment type
+                # attachment_data[1] will be attachment path
+                attachment_data = attachment.split('_')
+                if len(attachment_data) < 2:
+                    print('Incorrect attachment "{}"'.format(attachment))
+                    continue
+                attachments.append(Attachment(
+                    attachment_type=attachment_data[0],
+                    attachment_path=attachment_data[1],
+                    attachment_id=randint(1, 1000000000)  # Fake id
+                ))
+
+        yield IncomingMessage(sender=User(username=getlogin()), message_text=text,
+                              message_attachments=attachments, variables={
+                                  'console_id': randint(1, 1000000000)  # Fake id
+                              })
 
 
 def send_message(Message, bot):
     pass
+
+
+# Every adapter must have implementation of User class.
+class User:
+    def __init__(self, username):
+        """
+        Create new User for console adapter
+
+        :param username: name in bash session
+        :return:
+        """
+        # Adapter object should have 'id' and 'username' params.
+        self.id = hash(username)
+        self.username = username
