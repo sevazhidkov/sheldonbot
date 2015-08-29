fb5d0c18b6c5c7c7c5814ff3fb939aff42e72640
Seva Zhidkov <zhidkovseva@gmail.com>
2015-08-29 08:42:15 +0400

    Add empty storage class

---
+++ sheldon/storage.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Interface to Redis-storage.
+
+@author: Seva Zhidkov
+@contact: zhidkovseva@gmail.com
+@license: The MIT license
+
+Copyright (C) 2015
+"""
+
+
+class Storage:
+    pass
