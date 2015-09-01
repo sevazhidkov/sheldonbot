00cf105bd55f03bbaa98f21efca56aa6016e2a1a
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-01 18:20:44 +0400

    Delete int function wrapper for redis arguments

--- sheldon/storage.py
+++ sheldon/storage.py
@@ -30,17 +30,11 @@ class Storage:
         # Not redis-required modules must work without Redis.
         try:
             # We get parameters for redis connection from bot's config.
-            # Redis port and redis db must be integers, so we give
-            # third argument to bot.config.get function with
-            # wrapper-function.
-            self.redis = StrictRedis(host=bot.config.get('SHELDON_REDIS_HOST', 'localhost'),
-                                     port=bot.config.get(
-                                         'SHELDON_REDIS_PORT', 6379, int
-                                     ),
-                                     db=bot.config.get(
-                                         'SHELDON_REDIS_DB', 0, int
-                                     )
-                                    )
+            self.redis = StrictRedis(
+                host=bot.config.get('SHELDON_REDIS_HOST', 'localhost'),
+                port=bot.config.get('SHELDON_REDIS_PORT', '6379'),
+                db=bot.config.get('SHELDON_REDIS_DB', '0')
+            )
         except Exception as error:
             logger.error_log_message('Error while connecting Redis:')
             logger.error_log_message(str(error.__traceback__))
