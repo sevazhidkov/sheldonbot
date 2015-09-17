2691c4abcbeb160bebc8b4fbd435a8ec2f808b5b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-17 15:59:07 +0400

    Add initialization of bot config

--- sheldon/config.py
+++ sheldon/config.py
@@ -12,4 +12,21 @@ import os
 
 
 class Config:
-    pass
+    def __init__(self, prefix='SHELDON_'):
+        """
+        Load config from environment variables.
+
+        :param prefix: str, all needed environment variables
+                            starts from it.
+                            Default - 'SHELDON_'. So, environment
+                            variables will be looking like:
+                            'SHELDON_BOT_NAME', 'SHELDON_TWITTER_KEY'
+        :return:
+        """
+        # Bot config variables
+        self.variables = {}
+
+        for variable in os.environ:
+            if variable.startswith(prefix):
+                self.variables[variable] = os.environ[variable]
+
