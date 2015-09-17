ce1fb05e825e9be7589fd12ab798cae760b605e6
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-17 07:31:09 +0400

    Add basic load config function

--- sheldon/bot.py
+++ sheldon/bot.py
@@ -21,14 +21,28 @@ class Sheldon:
     Run script creating new instance of this class and run it.
     """
 
-    def __init__(self):
+    def __init__(self, command_line_arguments):
         """
         Function for loading bot.
 
+        :param command_line_arguments: dict, arguments for start script
         :return:
         """
 
-        # Creating empty lists for plugins and adapters
-        self.plugins = []
-        self.adapters = []
+        self._load_config(command_line_arguments)
 
+    def _load_config(self, command_line_arguments):
+        """
+        Сreate and load bot config.
+
+        :param command_line_arguments: dict, arguments for creating config:
+                                       config-prefix - prefix of environment
+                                                       variables.
+                                                       Default - 'SHELDON_'
+        :return:
+        """
+        # Config class is imported from sheldon.config
+        if 'config-prefix' in command_line_arguments:
+            self.config = Config(prefix=command_line_arguments['config-prefix'])
+        else:
+            self.config = Config()
