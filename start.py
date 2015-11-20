a0a60f8b395cc4b90dc550199acba5ed58e27ddd
Seva Zhidkov <zhidkovseva@gmail.com>
2015-11-20 14:14:47 +0400

    Add console arguments parsing to start script

--- start.py
+++ start.py
@@ -1,7 +1,15 @@
+import argparse
 from sheldon import Sheldon
 
-# Simulate command line arguments
-# TODO
-bot = Sheldon({'config-prefix': 'SHELDON_',
-               'adapter': 'console'})
-print(bot.start())
+parser = argparse.ArgumentParser(description='Start bot')
+parser.add_argument('--config-prefix', type=str, default='SHELDON_',
+                    help='a str from which starting all config variables')
+parser.add_argument('--adapter', type=str, default='console',
+                    help='a str with name of adapter from adapters folder'
+                         'or PyPi')
+args = parser.parse_args()
+
+bot = Sheldon({'config-prefix': args.config_prefix,
+               'adapter': args.adapter})
+
+bot.start()
